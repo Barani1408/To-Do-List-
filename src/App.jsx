5bac47f3029028c4cfd033849006ed838bf43692
Barani1408 <baranib2k@gmail.com>
2024-11-15 20:30:11 +0530

    To-Do-App using localstorage

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Todo from './components/Todo'
+
+function App() {
+
+  return (
+    <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 grid py-4 h-screen'>
+     <Todo/>
+    </div>
+  )
+}
+
+export default App
